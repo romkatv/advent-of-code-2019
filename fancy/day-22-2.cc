5bd8ceea303e8b3f4c04b5b91857e7f0806ef692
romkatv <roman.perepelitsa@gmail.com>
2019-12-23 23:56:00 +0100

    cleanup

--- fancy/day-22-2.cc
+++ fancy/day-22-2.cc
@@ -2,28 +2,27 @@
 #include <iostream>
 #include <string>
 
+const int64_t m = 119315717514047, n=101741582076661, needle=2020;
+
+// https://en.wikipedia.org/wiki/Exponentiation_by_squaring
+const auto combine = [](auto f, int64_t unit, int64_t a, int64_t b) {
+  for (int64_t r = unit;; b >>= 1, a = f(a, a)) {
+    if (!b) return r;
+    if (b & 1) r = f(r, a);
+  }
+};
+
+static int64_t add(int64_t a, int64_t b) { return (m + (a + b) % m) % m; }  // +  (mod m)
+static int64_t mul(int64_t a, int64_t b) { return combine(add, 0, a, b); }  // *  (mod m)
+static int64_t pow(int64_t a, int64_t b) { return combine(mul, 1, a, b); }  // ** (mod m)
+
 int main() {
-  const int64_t m = 119315717514047, n=101741582076661, needle=2020;
-
-  // https://en.wikipedia.org/wiki/Exponentiation_by_squaring
-  auto combine = [=](auto f, int64_t unit, int64_t a, int64_t b) {
-    for (int64_t r = unit;; b >>= 1, a = f(a, a)) {
-      if (!b) return r;
-      if (b & 1) r = f(r, a);
-    }
-  };
-
-  auto add = [&](int64_t a, int64_t b) { return (m + (a + b) % m) % m; };  // +  (mod m)
-  auto mul = [&](int64_t a, int64_t b) { return combine(add, 0, a, b); };  // *  (mod m)
-  auto pow = [&](int64_t a, int64_t b) { return combine(mul, 1, a, b); };  // ** (mod m)
-
   int64_t k = 1, b = 0, x;
   for (std::string s; std::getline(std::cin, s);) {
     if (s.find("inc") + 1) { k = mul(k, x = std::stoll(s.substr(20))); b = mul(b, x)  ; }
     if (s.find("cut") + 1) { b = add(b, -std::stoll(s.substr(4)))    ; b = b          ; }
     if (s.find("new") + 1) { k = add(0, -k)                          ; b = add(-1, -b); }
   }
-
   x=mul(b, pow(k-1, m-2));  // compute (λ c => k*c + b)**-n and feed it needle
   std::cout << add(mul(add(x, needle), pow(pow(k, m-2), n)), -x) << std::endl;
 }
