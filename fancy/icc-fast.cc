241ddbb3fa0be5565a7428d62887a50a2e6ca543
romkatv <roman.perepelitsa@gmail.com>
2020-01-05 09:45:29 +0100

    optimize: manual register allocation and mmap at zero address

--- fancy/icc-fast.cc
+++ fancy/icc-fast.cc
@@ -14,9 +14,9 @@
 #include <iostream>
 #include <sstream>
 
-static void(*ops[22209])();
-static int64_t pc, base, *mem = static_cast<int64_t*>(mmap(
-    0, 1LL << 46, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON | MAP_NORESERVE, 0, 0));
+constexpr int64_t* z = nullptr;
+register void(**ops)() asm("r13");
+register int64_t *pc asm("r14"), *base asm("r15");
 
 template <int M = -1, int... N, class F, class... T>
 void op(int opcode, F f, void (*)(T...) = 0) {
@@ -25,8 +25,8 @@ void op(int opcode, F f, void (*)(T...) = 0) {
   } else if constexpr (sizeof...(N) == sizeof...(T)) {
     ops[100 * M + opcode] = +[] {
       pc += sizeof...(N) + 1;
-      (+*(F*)(0))(N & 8 ? mem[pc - N % 4] : mem[mem[pc - N % 4] + N / 4 * base]...);
-      ops[mem[pc]]();
+      (+*(F*)(0))(N & 8 ? pc[-N % 4] : (N & 4 ? base : +z)[pc[-N % 4]]...);
+      ops[*pc]();
     };
   } else {
     op<M * 10 + 0, sizeof...(N) + 1, N...>(opcode, f, +f);
@@ -36,16 +36,19 @@ void op(int opcode, F f, void (*)(T...) = 0) {
 }
 
 int main(int argc, char** argv) {
+  ops = [] { static void(*ops[22209])(); return ops; }();
   op( 1, [](int64_t a, int64_t b, int64_t& r) { r = a + b             ; });  // add
   op( 2, [](int64_t a, int64_t b, int64_t& r) { r = a * b             ; });  // mul
   op( 7, [](int64_t a, int64_t b, int64_t& r) { r = a < b             ; });  // lt
   op( 8, [](int64_t a, int64_t b, int64_t& r) { r = a == b            ; });  // eq
-  op( 5, [](int64_t a, int64_t b            ) { pc = a ? b : pc       ; });  // jnz
-  op( 6, [](int64_t a, int64_t b            ) { pc = a ? pc : b       ; });  // jz
+  op( 5, [](int64_t a, int64_t b            ) { if (a) pc = z + b     ; });  // jnz
+  op( 6, [](int64_t a, int64_t b            ) { if (!a) pc = z + b    ; });  // jz
   op( 9, [](int64_t a                       ) { base += a             ; });  // rel
   op( 4, [](int64_t a                       ) { std::cout << a << '\n'; });  // out
   op( 3, [](                      int64_t& r) { std::cin >> r         ; });  // in
   op(99, [](                                ) { std::exit(0)          ; });  // hlt
-  for (std::istringstream ss(argv[1]); ss >> mem[pc++]; ss.get()) {}
-  ops[mem[pc = 0]]();
+  pc = base = (int64_t*)mmap(0, 1LL << 46, PROT_READ | PROT_WRITE,
+                             MAP_PRIVATE | MAP_ANON | MAP_NORESERVE | MAP_FIXED, 0, 0);
+  for (std::istringstream ss(argv[1]); ss >> *pc++; ss.get()) {}
+  ops[*(pc = base)]();
 }
