f6e22df33107c0a0662b500a0f874cdd07961c55
romkatv <roman.perepelitsa@gmail.com>
2020-01-20 17:49:35 +0100

    cleanup

--- fancy/day-22-2.cc
+++ fancy/day-22-2.cc
@@ -18,9 +18,9 @@ static int64_t pow(int64_t a, int64_t b) { return combine(mul, 1, a, b); }
 int main() {
   int64_t k = 1, b = 0, x;
   for (std::string s; std::getline(std::cin, s);) {
-    if (s.find("inc") + 1) { k = mul(k, x = std::stoll(s.substr(20))); b = mul(b, x)  ; }
-    if (s.find("cut") + 1) { b = add(b, -std::stoll(s.substr(4)))    ; b = b          ; }
     if (s.find("new") + 1) { k = add(0, -k)                          ; b = add(-1, -b); }
+    if (s.find("inc") + 1) { k = mul(k, x = std::stoll(s.substr(20))); b = mul(b, x)  ; }
+    if (s.find("cut") + 1) { b = add(b, -std::stoll(s.substr(4)))                     ; }
   }
   x=mul(b, pow(k-1, m-2));
   std::cout << add(mul(add(x, needle), pow(pow(k, m-2), n)), -x) << std::endl;
